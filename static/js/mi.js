61be2dd0604d716cf75a41edf2578817f8f0a2f5
Hugo Caille <hugo.caille@isen-lille.fr>
2013-12-23 23:35:17 +0100

    Added checkout feature.

--- static/js/mi.js
+++ static/js/mi.js
@@ -1,20 +1,31 @@
+current = {
+	account_id : undefined,
+};
+
 function getTransactions(account_id) {
 	var table = $("#transactionList");
+	table.children().remove();
 	
 	function refresh(data) {
 		var data = data.data;
 		
 		for (var i=0; i < data.length; i++) {
-			var data = data[i];
-			
 			var line = $("<tr>").appendTo(table);
-			if (data.balance <= 0) { line.addClass("warning"); }
-			if (data.balance < -10) { line.addClass("error"); }
-			if (data.balance > 0) { line.addClass("success"); }
+			if (data[i].balance < -10) { 
+				line.addClass("danger");
+			}
+			else {
+				if (data[i].balance <= 0) { 
+					line.addClass("warning");
+				}
+				else { 
+					line.addClass("success");
+				}
+			}
 			
-			$("<td>").html(data.operation).appendTo(line);
-			$("<td>").html(data.cash).appendTo(line);
-			$("<td>").html(data.balance).appendTo(line);
+			$("<td>").html(data[i].operation).appendTo(line);
+			$("<td>").html(data[i].cash).appendTo(line);
+			$("<td>").html(data[i].balance).appendTo(line);
 		}
 	}
 	
@@ -30,13 +41,30 @@ function getAccount() {
 		$("#name").html(data.firstname + " " + data.lastname);
 		$("#year").html(data.promo);
 		
+		current.account_id = data.id;
 		getTransactions(data.id);
 	}
 	
 	api.account.getByNumber(refresh, number);
 }
 
+function checkout() {
+	var account_id = current.account_id;
+	var cash = $("#checkout #directInput").val();
+	
+	function refresh(data) {
+		getTransactions(account_id);
+	}
+	
+	api.transaction.add(refresh, account_id, cash);
+}
+
 $("#searchByIDForm").submit(function(ev) {
 	ev.preventDefault();
 	getAccount();
+});
+
+$("#checkout").submit(function(ev) {
+	ev.preventDefault();
+	checkout();
 });
