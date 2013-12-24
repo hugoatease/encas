0ea7c963ca8ed39b02519683c0b8320297315083
Hugo Caille <hugo.caille@isen-lille.fr>
2013-12-24 12:36:49 +0100

    Refactoring accounts list function

--- static/js/accounts_list.js
+++ static/js/accounts_list.js
@@ -1,38 +1,19 @@
 function displayAccountList () {
-
 	function success(data) {
 		if (data.error) {
 			result.html("Erreur: " + data.reason);
 			return;
 		}
 
-		hisTableE = $('#hisTable')
-
-		var tableHtml = ""
-
+		var table = $('#hisTable');
 		for (var i = 0; i <= data.data.length -1 ; i++) {
-
-			var tableHtmlB = ""
-
-			var level = " id = \"success\" "
-			
-			// if (acc.history[i][2] > 0 ){level = " class = \"success\" "};
-			// if (acc.history[i][2] <= 0 ){level = " class = \"warning\" "};
-			// if (acc.history[i][2] < -10){level = " class = \"error\" "};
-
-			tableHtml = tableHtml.concat("<tr" + level + "><td>" + data.data[i].number + "</td> <td>" + data.data[i].firstname +" " + data.data[i].lastname + "</td> </tr>")
+			var line = $("<tr>").appendTo(table);
+			$("<td>").html(data.data[i].number).appendTo(line);
+			$("<td>").html(data.data[i].firstname + " " + data.data[i].lastname).appendTo(line);
 		}
- 
-		hisTableE.html(tableHtml)
-
-		console.log(data)
-		console.log(tableHtml)
-		console.log(data.data[0].firstname)
-
 	}
 
-	api.account.list(success)
-
+	api.account.list(success);
 }
 
-displayAccountList()
+displayAccountList();
