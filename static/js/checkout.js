b77ac186c9e22b7f1dfe026fd4373f6538807d10
Hugo Caille <hugo.caille@isen-lille.fr>
2014-01-10 21:32:09 +0100

    Refactored checkout with KnockoutJS

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,76 +1,61 @@
-current.search_callback = getTransactions;
-
-function getTransactions(account_id) {
-	var table = $("#transactionList");
-	table.children().remove();
+var checkoutModel = {
+	balance: ko.observable(),
+	transactions: ko.observableArray(),
+	checkout_price: ko.observable(),
 	
-	function refresh(data) {
-		if (reportError(data)) {
-			return;
-		}
-		
-		var data = data.data;
-		
-		for (var i=0; i < data.length; i++) {
-			var line = $("<tr>").appendTo(table);
-			if (data[i].balance < -10) { 
-				line.addClass("danger");
+	getTransactions: function(account_id) {
+		function refresh(data) {
+			if (reportError(data)) {
+				return;
 			}
-			else {
-				if (data[i].balance <= 0) { 
-					line.addClass("warning");
+			
+			var data = data.data;
+			
+			for (var i=0; i < data.length; i++) {
+				data[i].date = formatDate(new Date(data[i].date));
+				
+				if (data[i].balance < -10) { 
+					data[i]['state'] = "danger";
 				}
-				else { 
-					line.addClass("success");
+				else {
+					if (data[i].balance <= 0) { 
+						data[i]['state'] = "warning";
+					}
+					else { 
+						data[i]['state'] = "success";
+					}
 				}
 			}
-			
-			var date = new Date(data[i].date);
-			
-			$("<td>").html(data[i].operation).appendTo(line);
-			$("<td>").html(formatDate(date)).appendTo(line);
-			$("<td>").html(data[i].cash + " €").appendTo(line);
-			$("<td>").html(data[i].balance + " €").appendTo(line);
-			
-			if (!data[i].revoked) {
-				$("<td>").append($("<span>").addClass("label").addClass("label-success").html("Non")).appendTo(line);
-			}
-			else {
-				$("<td>").append($("<span>").addClass("label").addClass("label-danger").html("Oui")).appendTo(line);
-			}
-		}
-	}
 	
-	function refreshBalance(data) {
-		if (reportError(data)) {
-			return;
+			this.transactions(data);
 		}
 		
-		var balance = data.data.balance;
-		$("#account_balance").html(balance);
-	}
-	
-	api.transaction.listByAccount(refresh, account_id);
-	api.account.balance(refreshBalance, account_id);
-}
-
-function checkout() {
-	var account_id = current.account_id;
-	var cash = $("#checkout #directInput").val();
-	
-	function refresh(data) {
-		if (reportError(data)) {
-			return;
+		function refreshBalance(data) {
+			if (reportError(data)) {
+				return;
+			}
+			
+			this.balance(data.data.balance);
 		}
-
-		getTransactions(account_id);
-		$("#checkout #directInput").val("");
-	}
+		
+		api.transaction.listByAccount(refresh.bind(checkoutModel), account_id);
+		api.account.balance(refreshBalance.bind(checkoutModel), account_id);
+	},
 	
-	api.transaction.add(refresh, account_id, cash);
-}
+	checkout: function() {
+		var account_id = current.account_id;
+		
+		function refresh(data) {
+			if (reportError(data)) {
+				return;
+			}
+	
+			this.getTransactions(account_id);
+			this.checkout_price("");
+		}
+		
+		api.transaction.add(refresh.bind(checkoutModel), account_id, checkoutModel.checkout_price);
+	}
+};
 
-$("#checkout").submit(function(ev) {
-	ev.preventDefault();
-	checkout();
-});
+current.search_callback = checkoutModel.getTransactions;
