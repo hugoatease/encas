8be084c21f1f99dc706b7dd631cb2310ce8d61e9
Hugo Caille <hugo.caille@isen-lille.fr>
2014-01-28 19:44:09 +0100

    transactionModel and bindings refactoring

--- static/js/transaction.js
+++ static/js/transaction.js
@@ -1,61 +1,68 @@
-var transactionModel = {
-    transactions: ko.observableArray(),
-    show_revoke: ko.observable(true),
-    show_account : ko.observable(false),
+transactionModel = function(current, ko, api, accountModel) {
+    var module = {
+        transactions: ko.observableArray(),
+        show_revoke: ko.observable(true),
+        show_account : ko.observable(false),
 
-    show_all : true,
+        show_all : true,
 
-    getTransactions: function(account_id, all) {
-		function refresh(data) {
-			if (reportError(data)) {
-				return;
-			}
+        getTransactions: function(account_id, all) {
+            function refresh(data) {
+                if (reportError(data)) {
+                    return;
+                }
 
-			var data = data.data;
+                var data = data.data;
 
-			for (var i=0; i < data.length; i++) {
-				data[i].date = formatDate(new Date(data[i].date));
-                data[i]['state'] = getAccountState(data[i].balance);
-			}
+                for (var i=0; i < data.length; i++) {
+                    data[i].date = formatDate(new Date(data[i].date));
+                    data[i]['state'] = getAccountState(data[i].balance);
+                }
 
-			transactionModel.transactions(data);
-		}
-
-        if (all === true) {
-            api.transaction.list(refresh.bind(transactionModel));
-        }
-        else {
-		    api.transaction.listByAccount(refresh.bind(transactionModel), account_id, transactionModel.show_all);
-        }
-	},
-
-    clear : function() {
-        transactionModel.transactions.removeAll();
-    },
-
-    revoke : function(target) {
-        var transaction_id = target.id;
-
-        function refresh(data) {
-            if (reportError(data)) {
-                return;
+                this.transactions(data);
             }
-            var data = data.data;
 
-            reportSuccess("L'opération " + data.operation + " a été révoquée.");
-
-            if (current.account_id !== undefined) {
-                transactionModel.getTransactions(current.account_id);
+            if (all === true) {
+                api.transaction.list(refresh.bind(this));
             }
             else {
-                transactionModel.getTransactions(undefined, true);
+                api.transaction.listByAccount(refresh.bind(this), account_id, this.show_all);
+            }
+        },
+
+        clear : function() {
+            this.transactions.removeAll();
+        },
+
+        revoke : function(target) {
+            var transaction_id = target.id;
+
+            function refresh(data) {
+                if (reportError(data)) {
+                    return;
+                }
+                var data = data.data;
+
+                reportSuccess("L'opération " + data.operation + " a été révoquée.");
+
+                if (current.account_id !== undefined) {
+                    this.getTransactions(current.account_id);
+                }
+                else {
+                    this.getTransactions(undefined, true);
+                }
+
+                if (accountModel !== undefined) {
+                    accountModel.showAccountData(current.account_id);
+                }
             }
 
-            if (accountModel !== undefined) {
-                accountModel.showAccountData(current.account_id);
-            }
+            api.transaction.revoke(refresh.bind(this), transaction_id);
         }
+    };
 
-        api.transaction.revoke(refresh, transaction_id);
-    }
-};
+    module.revoke = module.revoke.bind(module);
+    module.getTransactions = module.getTransactions.bind(module);
+
+    return module;
+}
