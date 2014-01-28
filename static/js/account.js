0ae8e7625f714a2ba8d8cd74c144b98d4fda4f3a
Hugo Caille <hugo.caille@isen-lille.fr>
2014-01-28 18:58:53 +0100

    accountModel into module

--- static/js/account.js
+++ static/js/account.js
@@ -1,112 +1,117 @@
-var accountModel = {
-	id: ko.observable(),
-	number : ko.observable(),
-	firstname: ko.observable(),
-	lastname: ko.observable(),
-	promo: ko.observable(),
-	staff: ko.observable(),
-	date: ko.observable(),
-	balance: ko.observable(),
-	
-	edited: {
-		firstname: ko.observable(),
-		lastname: ko.observable(),
-		promo: ko.observable(),
-        staff: ko.observable()
-	},
-	
-	visible_intro: ko.observable(true),
-	visible_account_deletion: ko.observable(false),
-	visible_account_edition: ko.observable(false),
-	
-	showAccountData: function (account_id) {
-		current.account_id = account_id;
+accountModel = function(current, ko, api, transactionModel) {
+    var module = {
+        id: ko.observable(),
+        number : ko.observable(),
+        firstname: ko.observable(),
+        lastname: ko.observable(),
+        promo: ko.observable(),
+        staff: ko.observable(),
+        date: ko.observable(),
+        balance: ko.observable(),
 
-		this.visible_account_deletion(false);
-		this.visible_account_edition(false);
-	
-		function refreshAccount(data) {
-			if (reportError(data)) {
-				return;
-			}
-			var data = data.data;
-			
-			this.id(data.id);
-            this.number("#" + data.number);
-			this.firstname(data.firstname);
-			this.lastname(data.lastname);
-			this.promo(data.promo);
-			this.staff(data.staff);
-			
-			var date = new Date(data.creation);
-			this.date(formatDate(date));
+        edited: {
+            firstname: ko.observable(),
+            lastname: ko.observable(),
+            promo: ko.observable(),
+            staff: ko.observable()
+        },
 
-            this.visible_intro(false);
-		}
-		
-		function refreshBalance(data) {
-			if (reportError(data)) {
-				return;
-			}
-			var data = data.data;
-			
-			this.balance(data.balance);
-		}
-		
-		api.account.get(refreshAccount.bind(accountModel), current.account_id);
-		api.account.balance(refreshBalance.bind(accountModel), current.account_id, true);
-	},
+        visible_intro: ko.observable(true),
+        visible_account_deletion: ko.observable(false),
+        visible_account_edition: ko.observable(false),
 
-	showEditor : function(target) {
-		accountModel.visible_account_edition(true);
-		accountModel.edited.firstname(accountModel.firstname());
-		accountModel.edited.lastname(accountModel.lastname());
-		accountModel.edited.promo(accountModel.promo());
-        accountModel.edited.staff(accountModel.staff());
-	},
-	
-	edit : function(target) {
-		function refresh(data) {
-			if (reportError(data)) {
-				return;
-			}
-			var data = data.data;
-			accountModel.showAccountData(data.id);
-            showAccount(data.number);
-		}
+        showAccountData: function (account_id) {
+            current.account_id = account_id;
 
-        function staff_success(data) {
-            if (reportError(data)) {
-                return;
+            this.visible_account_deletion(false);
+            this.visible_account_edition(false);
+
+            function refreshAccount(data) {
+                if (reportError(data)) {
+                    return;
+                }
+                var data = data.data;
+
+                this.id(data.id);
+                this.number("#" + data.number);
+                this.firstname(data.firstname);
+                this.lastname(data.lastname);
+                this.promo(data.promo);
+                this.staff(data.staff);
+
+                var date = new Date(data.creation);
+                this.date(formatDate(date));
+
+                this.visible_intro(false);
             }
-        }
 
-        if (accountModel.edited.staff() !== accountModel.staff()) {
-            api.account.staff(staff_success, current.account_id, accountModel.edited.staff());
-        }
+            function refreshBalance(data) {
+                if (reportError(data)) {
+                    return;
+                }
+                var data = data.data;
 
-        api.account.edit(refresh, current.account_id, accountModel.edited.firstname(), accountModel.edited.lastname(), accountModel.edited.promo());
-	},
-
-    delete : function(target) {
-        function refresh(data) {
-            if (reportError(data)) {
-                return;
+                this.balance(data.balance);
             }
-            var data = data.data;
 
-            accountModel.visible_account_deletion(false);
-            accountModel.visible_intro(true);
-            transactionModel.clear();
+            api.account.get(refreshAccount.bind(this), current.account_id);
+            api.account.balance(refreshBalance.bind(this), current.account_id, true);
+        },
 
-            var message = "L'utilisateur " + data.lastname + " " + data.firstname + " a été supprimé"
-            reportSuccess(message);
+        showEditor : function(target) {
+            this.visible_account_edition(true);
+            this.edited.firstname(this.firstname());
+            this.edited.lastname(this.lastname());
+            this.edited.promo(this.promo());
+            this.edited.staff(this.staff());
+        },
+
+        edit : function(target) {
+            function refresh(data) {
+                if (reportError(data)) {
+                    return;
+                }
+                console.log(this);
+                var data = data.data;
+                this.showAccountData(data.id);
+                showAccount(data.number);
+            }
+
+            function staff_success(data) {
+                if (reportError(data)) {
+                    return;
+                }
+            }
+
+            if (this.edited.staff() !== this.staff()) {
+                api.account.staff(staff_success.bind(this), current.account_id, this.edited.staff());
+            }
+
+            api.account.edit(refresh.bind(this), current.account_id, this.edited.firstname(), this.edited.lastname(), this.edited.promo());
+        },
+
+        delete : function(target) {
+            function refresh(data) {
+                if (reportError(data)) {
+                    return;
+                }
+                var data = data.data;
+
+                this.visible_account_deletion(false);
+                this.visible_intro(true);
+                transactionModel.clear();
+
+                var message = "L'utilisateur " + data.lastname + " " + data.firstname + " a été supprimé"
+                reportSuccess(message);
+            }
+
+            api.account.delete(refresh.bind(this), current.account_id);
         }
+    };
 
-        api.account.delete(refresh, current.account_id);
-    }
-};
+    module.fullname = ko.computed((function() {
+        return this.lastname() + " " + this.firstname();
+    }).bind(module));
 
-accountModel.fullname = ko.computed(function() {
-	return accountModel.lastname() + " " + accountModel.firstname();
-});
+    return module;
+}
