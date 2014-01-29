703803ab266c3b7c82306a0081535122ee308dda
Hugo Caille <hugo.caille@isen-lille.fr>
2014-01-29 11:59:21 +0100

    Fixing js_admin template and Bootstrap tabs.

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 requirejs.config({
    baseUrl: 'static/js',
-   //urlArgs: "dev=" + (new Date()).getTime(),
+   urlArgs: "dev=" + (new Date()).getTime(),
 
    shim: {
        'mousetrap' : {
@@ -17,36 +17,4 @@ requirejs.config({
    }
 });
 
-requirejs(['require', 'knockout', 'mousetrap', 'encasapi', 'common', 'search', 'model/account', 'model/admin', 'model/checkout', 'model/transaction'],
-    function(require, ko, Mousetrap) {
-        var api = require('encasapi');
-        var search = require('search');
-        var common = require('common');
-        var accountModel = require('model/account');
-        var adminModel = require('model/admin');
-        var checkoutModel = require('model/checkout');
-        var transactionModel = require('model/transaction');
-
-        var viewModels = {
-            checkout : checkoutModel,
-            account : accountModel,
-            transaction : transactionModel,
-            admin : adminModel,
-            is_admin : ko.observable()
-        };
-
-        viewModels.account.deleteAcc = viewModels.account.delete;
-        viewModels.checkout.make_checkout = function() {
-                checkoutModel.checkout(transactionModel.getTransactions);
-        }
-
-        ko.applyBindings(viewModels);
-
-        Mousetrap.bindGlobal('escape', function(ev) {
-            ev.preventDefault();
-            search.boxes.number.hide();
-            search.boxes.name.hide();
-            checkoutModel.checkout_focus(false);
-        });
-    }
-);
+requirejs(['main']);
---
+++ static/js/main.js
@@ -0,0 +1,35 @@
+define(['require', 'knockout', 'mousetrap', 'encasapi', 'common', 'search', 'model/account', 'model/admin', 'model/checkout', 'model/transaction'],
+    function(require, ko, Mousetrap) {
+        var api = require('encasapi');
+        var search = require('search');
+        var common = require('common');
+        var accountModel = require('model/account');
+        var adminModel = require('model/admin');
+        var checkoutModel = require('model/checkout');
+        var transactionModel = require('model/transaction');
+
+        var viewModels = {
+            checkout : checkoutModel,
+            account : accountModel,
+            transaction : transactionModel,
+            admin : adminModel,
+            is_admin : ko.observable()
+        };
+
+        viewModels.account.deleteAcc = viewModels.account.delete;
+        viewModels.checkout.make_checkout = function() {
+                checkoutModel.checkout(transactionModel.getTransactions);
+        }
+
+        ko.applyBindings(viewModels);
+
+        Mousetrap.bindGlobal('escape', function(ev) {
+            ev.preventDefault();
+            search.boxes.number.hide();
+            search.boxes.name.hide();
+            checkoutModel.checkout_focus(false);
+        });
+
+        return viewModels;
+    }
+);
